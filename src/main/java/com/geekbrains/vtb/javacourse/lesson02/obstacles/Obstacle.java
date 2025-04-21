b18353fb1bc6b9d5aa5619edb89d1c328b1c08fa
anastasiayildirim <anastasia.skrydlova@gmail.com>
2025-04-21 21:22:29 +0300

    Revert "added homework"
    This reverts commit e8726a76d6406d656e18dacde1555eed6055ff5c.

--- src/main/java/com/geekbrains/vtb/javacourse/lesson02/obstacles/Obstacle.java
+++
@@ -1,7 +0,0 @@
-package com.geekbrains.vtb.javacourse.lesson02.obstacles;
-
-import com.geekbrains.vtb.javacourse.lesson02.participants.Participant;
-
-public interface Obstacle {
-    boolean challenge(Participant participant);
-}
--- src/main/java/com/geekbrains/vtb/javacourse/lesson02/participants/Participant.java
+++
@@ -1,6 +0,0 @@
-package com.geekbrains.vtb.javacourse.lesson02.participants;
-
-public interface Participant {
-    boolean jump(int centimeters);
-    boolean run(int meters);
-}
